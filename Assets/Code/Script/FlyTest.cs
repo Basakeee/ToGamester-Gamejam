cd04793c85bb1f09f921a04f9b1cf974c30a0973
xFallerz <30279576+Dosyuki@users.noreply.github.com>
2024-01-27 10:33:57 +0700

    Enemy AI

--- Assets/Code/Script/FlyTest.cs
+++ Assets/Code/Script/FlyTest.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestAi : MonoBehaviour
+public class FlyTest : MonoBehaviour
 {
     public Transform pointA;
     public Transform pointB;
     public Transform CurrentPoint;
     public Rigidbody2D rb => GetComponent<Rigidbody2D>();
     public float speed;
-    public LayerMask mask,ground,player;
+    public LayerMask mask,player;
     public float PlayerDetectRange;
-    public float WallDetectRange;
-    public float GroundDetectRange;
-    public float JumpPower;
-    public float jumpcooldown;
     public float range;
     public int dmg;
     public float atkCooldown;
@@ -26,14 +22,10 @@ public class TestAi : MonoBehaviour
         Chase
     }
     public State enemyState;
-
-    private bool canJump = true;
     private Collider2D playerDetect => Physics2D.OverlapCircle(transform.position, PlayerDetectRange, mask);
     private Collider2D playerinAttackRange => Physics2D.OverlapCircle(transform.position + (off * -transform.localScale.x), range, player);
     private int isFacingRight => transform.localScale.x > 0 ? -1 : 1;
 
-    private bool wallDetect => Physics2D.Raycast(transform.position, transform.right * isFacingRight, WallDetectRange, ground);
-    private bool groundDetect => Physics2D.Raycast(transform.position, -transform.up,GroundDetectRange, ground);
     private bool isPatrol = true;
     private bool faceRight;
     private bool isATK;
@@ -41,7 +33,6 @@ public class TestAi : MonoBehaviour
     void Start()
     {
         CurrentPoint = pointB;
-        canJump = true;
     }
 
     // Update is called once per frame
@@ -51,13 +42,6 @@ public class TestAi : MonoBehaviour
             Route();
 
         FindPlayer();
-        ShowRay();
-    }
-
-    private void ShowRay()
-    {
-        Debug.DrawRay(transform.position, transform.right * isFacingRight * WallDetectRange,Color.red);
-        Debug.DrawRay(transform.position, -transform.up * GroundDetectRange,Color.magenta);
     }
 
     private void FindPlayer()
@@ -67,7 +51,8 @@ public class TestAi : MonoBehaviour
             isPatrol = false;
             ChasePlayer();
             enemyState = State.Chase;
-            if(playerinAttackRange != null) // in range can attack
+            
+            if (playerinAttackRange != null) // in range can attack
             {
                 AttackPlayer();
             }
@@ -92,23 +77,12 @@ public class TestAi : MonoBehaviour
 
     private void ChasePlayer()
     {
-        if (transform.position.x > playerDetect.transform.position.x)
-        {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-            if (faceRight)
+        Vector2 direction = new Vector2(playerDetect.transform.position.x - transform.position.x, playerDetect.transform.position.y - transform.position.y).normalized;
+        rb.velocity = direction * speed;
+        if (faceRight && transform.position.x > playerDetect.transform.position.x)
                 Flip();
-        }
-        if (transform.position.x < playerDetect.transform.position.x)
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-            if (!faceRight)
+            if (!faceRight && transform.position.x < playerDetect.transform.position.x)
                 Flip();
-        }
-        if (wallDetect && groundDetect && canJump)
-        {
-            rb.AddForce(transform.up * JumpPower, ForceMode2D.Impulse);
-            StartCoroutine(jumpCD(jumpcooldown));
-        }
     }
 
     private void Route()
@@ -116,11 +90,12 @@ public class TestAi : MonoBehaviour
         enemyState = State.Patrol;
         if (CurrentPoint == pointB)
         {
-            if(faceRight)
+            if (faceRight)
             {
                 Flip();
             }
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            Vector2 direction = new Vector2(pointA.position.x - transform.position.x, pointA.position.y - transform.position.y).normalized;
+            rb.velocity = direction * speed;
             if (Vector2.Distance(transform.position, pointA.position) < 1f)
             {
                 CurrentPoint = pointA;
@@ -133,7 +108,8 @@ public class TestAi : MonoBehaviour
             {
                 Flip();
             }
-            rb.velocity = new Vector2(speed, rb.velocity.y);
+            Vector2 direction = new Vector2(pointB.position.x - transform.position.x, pointB.position.y - transform.position.y).normalized;
+            rb.velocity = direction * speed;
             if (Vector2.Distance(transform.position, pointB.position) < 1f)
             {
                 CurrentPoint = pointB;
@@ -155,12 +131,6 @@ public class TestAi : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, PlayerDetectRange);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position + (off * -transform.localScale.x), range);
-    }
-    IEnumerator jumpCD(float CD)
-    {
-        canJump = false;
-        yield return new WaitForSeconds(CD);
-        canJump = true;
     }
     IEnumerator AttackCD(float CD)
     {
