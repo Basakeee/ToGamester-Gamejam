aa9a89ea415d3bad326299a1cceab9f34fb74671
Basakeee <5559921to@gmail.com>
2024-01-28 08:17:22 +0700

    Update

---
+++ Assets/Scenes/SpikeRockScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeRockScript : MonoBehaviour
+{
+    public Vector2 Area;
+    public Vector2 offset;
+    public LayerMask mask;
+    private Vector3 off => offset;
+    private Collider2D hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hit = Physics2D.OverlapBox(transform.position + (off * transform.localScale.x), Area, 0, mask);
+        if (hit != null)
+        {
+            if (hit.TryGetComponent<Combat>(out Combat player))
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position + (off * transform.localScale.x), Area);
+    }
+}
